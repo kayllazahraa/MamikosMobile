5c07d1a22284ae1f218a6d15460652a282c12d99
Kaylla Zahraa <kayllazahrani@gmail.com>
2026-01-13 09:55:44 +0700

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,26 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(libs.androidx.compose.runtime)
 
+    // Retrofit untuk API
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // RecyclerView
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+        implementation("androidx.cardview:cardview:1.0.0")
+
+    // Material Design
+        implementation("com.google.android.material:material:1.11.0")
+
+    // ViewModel & LiveData
+        implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
+        implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
+
+    // Glide untuk gambar
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+        implementation(libs.androidx.material3)
+         implementation("androidx.compose.material:material-icons-extended")
+
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
